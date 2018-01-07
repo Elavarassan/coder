62fff7c5302b6a2938fa688fe5546d8403ab5c76
Elavarassan <34613548+Elavarassan@users.noreply.github.com>
2018-01-07 12:35:45 +0530

    Create positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	long int i;
+	scanf("%ld",&i);
+	if(i<=0.0)
+	{
+		if(i==0.0)
+		printf("entered value is zero it both positive and negative\t");
+		else
+		printf("NEGATIVE\t");
+	}
+	else
+	{
+		printf("POSITIVE\t");
+	}
+	return 0;
+}
