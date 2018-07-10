2bdd93fc6ef8ac3e3cd06fa01abc11fa015f4e1f
Elavarassan <34613548+Elavarassan@users.noreply.github.com>
2018-07-10 06:11:29 +0530

    Create guvi8.java

---
+++ guvi8.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class guvi8
+{
+    public static void main(String arg[])
+    {
+       Scanner sc=new Scanner(System.in);
+       String s=sc.next();
+       System.out.print(s.length());
+       
+    }
+}
+
+
+
