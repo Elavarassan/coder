fe34ef09673ef99ef0b83c74af5da1fbdddc8a41
Elavarassan <34613548+Elavarassan@users.noreply.github.com>
2018-07-10 06:07:51 +0530

    Create guvi4.java

---
+++ guvi4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class guvi4
+{
+    public static void main(String arg[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       if(a%4==0)
+       {
+           System.out.print("yes");
+       }
+       else
+       {
+           System.out.print("no");
+       }
+    }
+}
+
+
+
