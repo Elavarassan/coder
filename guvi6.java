13821582f9c347ceabf62019b46330d2e2fe4ea5
Elavarassan <34613548+Elavarassan@users.noreply.github.com>
2018-07-10 06:09:19 +0530

    Create guvi6.java

---
+++ guvi6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class guvi6
+{
+    public static void main(String arg[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int sum=0;
+       for(int i=1;i<=a;i++)
+       {
+           sum=sum+i;
+       }
+       System.out.print(sum);
+       
+    }
+}
+
+
+
