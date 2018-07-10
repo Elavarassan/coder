718588207cfc5c19d94dae9f28e6a030922b516e
Elavarassan <34613548+Elavarassan@users.noreply.github.com>
2018-07-10 06:08:37 +0530

    Create guvi5.java

---
+++ guvi5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class guvi5
+{
+    public static void main(String arg[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       for(int i=1;i<=a;i++)
+       {
+           System.out.println("Hello");
+       }
+       
+    }
+}
+
+
+
